3478b35c4e3b88203bb73bbb589cc9c167b779a3
lzzy12 <jhashivam2020@gmail.com>
2019-03-18 16:00:45 +0800

    Main : Add linux paths for testing
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- cli/Main.cpp
+++ cli/Main.cpp
@@ -2,7 +2,20 @@
 #include "Utils.h"
 int main(int argc, char **argv) {
 	cs::zipper zip;
+	std::string zipPath;
+#ifdef  CS_PLATFORM_WINDOWS
 	zip.addFile("C:\\dev\\CompressStudio\\CMakeLists.txt");
 	zip.addFile("C:\\dev\\CompressStudio\\README.md");
-	return zip.compress(std::string("C:\\dev\\CompressedStudio\\CS.zip"), true);
+	zipPath = "C:\\dev\\CompressStudio\\build\\test.zip";
+#endif //  CS_PLATFORM_WINDOWS
+#ifdef CS_PLATFORM_LINUX
+	zip.addFile("~/CompressStudio/CMakeLists.txt");
+	zip.addFile("~/CompressStudio/README.md");
+	zipPath = "~/CompressStudio/test.zip";
+#endif // CS_PLATFORM_LINUX
+
+	if (zip.compress(zipPath, true))
+		return 0;
+	else
+		return -1;
 }
